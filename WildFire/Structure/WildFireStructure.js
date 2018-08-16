8769cb6122cc99b88318af73697380d4a8183970
GodDragoner <goddragoner@gmail.com>
2018-08-16 22:19:36 +0200

    Update WildFireStructure.js
    Use a + to connect two strings or a  variable and a string

--- WildFire/Structure/WildFireStructure.js
+++ WildFire/Structure/WildFireStructure.js
@@ -1,5 +1,5 @@
 sendMessage("Getting a random number", 5);
-sendMessage("Getting a random number" 'randomInteger(1,500)', 5);
-sendMessage("Getting a random number" 'randomInteger(1,250)', 5);
-sendMessage("Getting a random number" 'randomInteger(1,125)', 5);
+sendMessage("Getting a random number" + randomInteger(1,500), 5);
+sendMessage("Getting a random number" + randomInteger(1,250), 5);
+sendMessage("Getting a random number" + randomInteger(1,125), 5);
 sendMessage("The end of WildFire Structure", 2);
