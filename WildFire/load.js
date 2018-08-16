df6e8b1fabe8aaf21923c117b892945d1ec88a3b
GodDragoner <goddragoner@gmail.com>
2018-08-16 14:30:50 +0200

    Update load.js
    Comments

--- WildFire/load.js
+++ WildFire/load.js
@@ -1,3 +1,5 @@
 let merits = 200;
-registerVariable("merits","Merits","Merits determine how popular you are with the Domme."); 
+registerVariable("merits","Merits","Merits determine how popular you are with the Domme.");
+//Yet again. merits = 200 only creates a variable in the code but not for TAJ. If you want to store it throughout multiple sessions use this:
+setVar("merits", 200);
 let strokePace = 60;
