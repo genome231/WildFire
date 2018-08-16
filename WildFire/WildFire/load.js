14256f39225ba061d38814724c6a32bf78a9e2fc
Genome <genome231@gmail.com>
2018-08-16 10:54:48 +0200

    Add files via upload

---
+++ WildFire/WildFire/load.js
@@ -0,0 +1,2 @@
+let merits = 200;
+registerVariable("merits","Merits","Merits determine how popular you are with the Domme."); 
---
+++ WildFire/WildFire/main.js
@@ -0,0 +1,8 @@
+sendMessage("Launching WildFire %slaveName%",2);
+sendMessage("running system functions");
+run("systemFunctions.js");
+sendMessage("running Installation");
+run("installation.js");
+sendMessage("running Structure");
+run("Structure/WildFireStructure.js");
+
